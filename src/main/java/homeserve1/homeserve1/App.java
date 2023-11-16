7006cf0ee7e6742377b480924fcedcaabe1e5693
arunn <arun.n.mech05@gmail.com>
2023-11-16 18:02:06 +0530

    adding

---
+++ src/main/java/homeserve1/homeserve1/App.java
@@ -0,0 +1,13 @@
+package homeserve1.homeserve1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
